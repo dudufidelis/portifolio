e15268ce1d9fd62840774eb9362d3e0c25c18dd6
dudufidelis <duduecal12@gmail.com>
2023-11-07 14:11:11 -0300

    .

--- js/scrollReveal.js
+++ js/scrollReveal.js
@@ -1,29 +1,26 @@
 ScrollReveal().reveal('.biography', {
     delay: 300,
-    reset: true,
     distance: '10%',
     origin: 'left'
 });
 
 ScrollReveal().reveal('.carrer-text', {
     delay: 500,
-    reset: true
 });
 
 ScrollReveal().reveal('.tecnologys', {
     delay: 500,
-    reset: true
 });
 
 ScrollReveal().reveal('.project-img', {
     delay: 300,
-    reset: true
 });
 
 ScrollReveal().reveal('.contact-content', {
     delay: 300,
-    reset: true
 });
+
+
 
 
 
